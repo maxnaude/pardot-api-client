72b2a62dc8f94d414d5f9a42ad23cd19f633e1c2
Max Naude <maxnaude@gmail.com>
2017-01-24 00:43:50 +0200

    Indented constants, added type testers to help distinguish between ambiguous resource parameters

--- pardot/constants.py
+++ pardot/constants.py
@@ -1,5 +1,12 @@
+import collections
+
+import six
+
+
 RESOURCE_OPERATIONS = {
-    'account': {'read': [()]},
+    'account': {
+        'read': [()]
+    },
     'campaign': {
         'create': [()],
         'query': [()],
@@ -13,22 +20,40 @@ RESOURCE_OPERATIONS = {
         'read': [('id', )],
         'update': [('id', )],
     },
-    'customRedirect': {'query': [()], 'read': [('id', )]},
-    'dynamicContent': {'query': [()], 'read': [('id', )]},
+    'customRedirect': {
+        'query': [()],
+        'read': [('id', )]
+    },
+    'dynamicContent': {
+        'query': [()],
+        'read': [('id', )]
+    },
     'email': {
         'read': [('email', )],
         'send': [
-            ('campaign_id', 'name', 'subject'),
             ('campaign_id', 'name', 'subject'),
             ('list_ids[]', 'campaign_id', 'name', 'subject'),
         ],
         'stats': [('email', )],
     },
-    'emailClick': {'query': [('search_criteria', 'result_set_criteria')]},
-    'emailTemplate': {'listOneToOne': [()], 'read': [('emailTemplateId', )]},
-    'form': {'query': [()], 'read': [('id', )]},
-    'lifecycleHistory': {'query': [()], 'read': [('id', )]},
-    'lifecycleStage': {'query': [()]},
+    'emailClick': {
+        'query': [('search_criteria', 'result_set_criteria')]
+    },
+    'emailTemplate': {
+        'listOneToOne': [()],
+        'read': [('emailTemplateId', )]
+    },
+    'form': {
+        'query': [()],
+        'read': [('id', )]
+    },
+    'lifecycleHistory': {
+        'query': [()],
+        'read': [('id', )]
+    },
+    'lifecycleStage': {
+        'query': [()]
+    },
     'list': {
         'create': [()],
         'delete': [('id', )],
@@ -43,7 +68,9 @@ RESOURCE_OPERATIONS = {
         'read': [('id', ), ('list_id', 'prospect_id')],
         'update': [('id', ), ('list_id', 'prospect_id')],
     },
-    'opportunity': {'query': [()]},
+    'opportunity': {
+        'query': [()]
+    },
     'opportunity ': {
         'create': [
             ('prospect_email', 'name', 'value', 'probability'),
@@ -54,7 +81,9 @@ RESOURCE_OPERATIONS = {
         'undelete': [('id', )],
         'update': [('id', )],
     },
-    'prospect': {'query': [()]},
+    'prospect': {
+        'query': [()]
+    },
     'prospect ': {
         'assign': [('email', ), ('id', )],
         'batchCreate': [('prospects', )],
@@ -75,13 +104,41 @@ RESOURCE_OPERATIONS = {
         'read': [('id', )],
         'update': [('id', )],
     },
-    'tag': {'query': [()], 'read': [('id', )]},
-    'tagObject': {'query': [()], 'read': [('id', )]},
-    'user': {'query': [()], 'read': [('email', ), ('id', )]},
-    'visit': {'query': [('visitor_ids', 'prospect_ids)')], 'read': [('id', )]},
-    'visitor': {'assign': [('id', )], 'query': [()], 'read': [('id', )]},
+    'tag': {
+        'query': [()],
+        'read': [('id', )]
+    },
+    'tagObject': {
+        'query': [()],
+        'read': [('id', )]
+    },
+    'user': {
+        'query': [()],
+        'read': [('email', ), ('id', )]
+    },
+    'visit': {
+        'query': [('visitor_ids', 'prospect_ids)')],
+        'read': [('id', )]
+    },
+    'visitor': {
+        'assign': [('id', )],
+        'query': [()],
+        'read': [('id', )]
+    },
     'visitorActivity': {
         'query': [('search_criteria', 'result_set_criteria')],
         'read': [('id', )],
     },
+}
+
+RESOURCE_PARAMETER_TYPE_TESTS = {
+    'ids': lambda x, y:
+        x == 'id' or x.endswith('_id') and
+        isinstance(y, six.integer_types),
+    'emails': lambda x, y:
+        x == 'email' or x.endswith('_email') and
+        isinstance(y, six.string_types),
+    'lists': lambda x, y:
+        x == x.endswith('[]') and isinstance(y, collections.Sequence) and
+        not isinstance(y, six.string_types),
 }
